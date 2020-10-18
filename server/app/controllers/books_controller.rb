0363ee7d84b4ec6a751b8dad89279140017ec548
Takashi Hayakawa <chou_22@icloud.com>
2020-10-18 23:34:18 +0900

    登録一覧をCORSで取得可能に

--- server/app/controllers/books_controller.rb
+++ server/app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :update, :destroy]
   def index
+    @books = Book.all
+    json_response(@books)
   end
 
   def show
@@ -8,6 +11,18 @@ class BooksController < ApplicationController
   def edit
   end
 
-  def new
+  def create
+    book = Book.new(book_paramas)
+    book.save!
+  end
+
+  private
+
+  def book_paramas
+    params.require(:books).permit(:name, :description, :favorite, :author, :category)
+  end
+
+  def set_book
+    @book = books.find(params[:id])
   end
 end
