ffbf593b1bbe42967c241cbab05cd23ff50c483d
Takashi Hayakawa <chou_22@icloud.com>
2020-10-18 23:35:34 +0900

    クライアント側：マウント先修正 / webpack-dev-server追加

--- client/src/views/index.tsx
+++ client/src/views/index.tsx
@@ -1,17 +1,23 @@
 import * as React from "react";
+import { useEffect } from "react";
 import * as ReactDOM from "react-dom";
+import axios from "axios";
 import { Router, Switch, Route, Link } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
 
-function Index(){
-  return (
-    <h1>welcome libreria app!!</h1>
-  );
+function Index() {
+  useEffect(() => {
+    axios.get("http://localhost:5555/books")
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log("faild");
+    });
+  }, []);
+  return <h1>welcome libreria app!!</h1>;
 }
 
-ReactDOM.render(
-  <Index />,
-  document.getElementById("app")
-)
+ReactDOM.render(<Index />, document.getElementById("app"));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,49 +1,56 @@
-// [定数] webpack の出力オプションを指定します
-// 'production' か 'development' を指定
+const path = require("path");
+const outputPath = path.resolve(__dirname, "src/views/public");
 const MODE = "development";
 
 // ソースマップの利用有無(productionのときはソースマップを利用しない)
 const enabledSourceMap = MODE === "development";
 
 module.exports = {
-	mode: MODE,
+  mode: MODE,
   entry: "./src/views/index.tsx",
   output: {
-      filename: "bundle.js",
-      path: __dirname + "/src/public"
+    filename: "bundle.js",
+    path: __dirname + "/src/views/public",
   },
 
   // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
 
   resolve: {
-      // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".ts", ".tsx", ".js", ".json"]
-	},
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+
+  devServer: {
+    host: "0.0.0.0",
+    port: 2525,
+    contentBase: outputPath,
+  },
 
   module: {
-      rules: [
-          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-          { test: /\.scss?$/, use: [
-              "style-loader",
-              {
-                  loader: "css-loader",
-                  options: {
-                      url: false,
-                      sourceMap: enabledSourceMap,
-                      importLoaders: 2
-                  }
-              },
-              {
-                  loader: "sass-loader",
-                  options: {
-                    sourceMap: enabledSourceMap
-                }
-              }
-          ] 
-        },
-
-      ]
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      {
+        test: /\.scss?$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+              sourceMap: enabledSourceMap,
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: enabledSourceMap,
+            },
+          },
+        ],
+      },
+    ],
   },
 };
