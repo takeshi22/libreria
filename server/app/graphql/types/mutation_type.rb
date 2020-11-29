3921c0c56eb1dbd60cf062b9cef9bae171f7bd0e
Takashi Hayakawa <chou_22@icloud.com>
2020-11-29 17:06:17 +0900

    ユーザー登録機能追加

--- server/app/graphql/types/mutation_type.rb
+++ server/app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :delete_book, mutation: Mutations::DeleteBook
     field :update_book, mutation: Mutations::UpdateBook
     field :create_book, mutation: Mutations::CreateBook
+    field :create_user, mutation: Mutations::CreateUser
   end
 end
